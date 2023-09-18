d3ce2026183399b4a39e8584659487762a22b7b4
Connor Dolley <88245714+connordolley@users.noreply.github.com>
2023-09-18 12:29:27 -0400

    Update main.py
    Reformatted code

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
+import os
+import random
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
-import random
 
-model = tf.keras.models.load_model("diabetes_prediction_model.h5")
+def gather_data():
+    print("Please provide the following information:")
+    pregnncy = int(input("Pregnancies: "))
+    glucose = int(input("Glucose: "))
+    blood_pressure = int(input("Blood Pressure: "))
+    skin_thickness = int(input("Skin thickness: "))
+    insulin = int(input("Insulin: "))
+    bmi = float(input("BMI: "))
+    diabetes_percentage = float(input("Diabetes Percentage: "))
+    age = int(input("Age: "))
 
-if __name__ == '__main__':
-    print("Diabetes prediction model.")
-    print("Pregnancies:")
-    p = int(input(""))
-    print("Glucose:")
-    g = int(input(""))
-    print("Blood Pressure:")
-    bp = int(input(""))
-    print("Skin thickness:")
-    s = int(input(""))
-    print("Insulin:")
-    i = int(input(""))
-    print("BMI:")
-    bmi = float(input(""))
-    print("Diabetes Percentage:")
-    dp = float(input(""))
-    print("Age:")
-    a = int(input(""))
+    patient_data = [pregnncy, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_percentage, age]
+    
+    return patient_data
 
-    result = model.predict([[p, g, bp, s, i, bmi, dp, a]])
+def analyze_data(model, patient_data):
+    result = model.predict([patient_data])
     
     if result > 0.5:
         result = "Positive for diabetes."
     else:
-        result = "Negative for diabetes"
+        result = "Negative for diabetes."
+    
+    return result
 
+def main():
+    print("Diabetes prediction model.")
+    model = tf.keras.models.load_model("diabetes_prediction_model.h5")
+    patient_data = gather_data()
+    result = analyze_data(model, patient_data)
     print("Result: ", result)
+
+if __name__ == '__main__':
+    main()
